ea77216f2531db166919627bfd7d0a725a29f90d
Chase Brignac <cjbrignac000@gmail.com>
2020-03-12 21:51:36 -0700

    This example works in curl fine but not swift for some reason

--- Dockerfile
+++ Dockerfile
@@ -41,10 +41,15 @@ RUN cd ~ && \
 #     pip3 install -r requirements.txt
 # RUN whatever_command_you_run_to_start_your_app
 
+RUN pwd
+RUN ls -al
+
+EXPOSE 5001
+
 COPY . /root/face_recognition
 RUN cd /root/face_recognition && \
     pip3 install -r requirements.txt && \
     python3 setup.py install
 
 CMD cd /root/face_recognition/examples && \
-    python3 recognize_faces_in_pictures.py
+    python3 web_service_example.py
--- examples/web_service_example.py
+++ examples/web_service_example.py
@@ -1,14 +1,14 @@
 # This is a _very simple_ example of a web service that recognizes faces in uploaded images.
-# Upload an image file and it will check if the image contains a picture of Barack Obama.
+# Upload an image file and it will check if the image contains the same face as another image.
 # The result is returned as json. For example:
 #
-# $ curl -XPOST -F "file=@obama2.jpg" http://127.0.0.1:5001
+# $ curl -XPOST -F "file_1=@obama1.jpg" -F "file_2=@obama2.jpg" http://127.0.0.1:5001
 #
 # Returns:
 #
 # {
 #  "face_found_in_image": true,
-#  "is_picture_of_obama": true
+#  "is_same_person": true
 # }
 #
 # This example is based on the Flask file upload example: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
@@ -32,80 +32,65 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
+    print("we are entering upload image")
     # Check if a valid image file was uploaded
+    print(request)
     if request.method == 'POST':
-        if 'file' not in request.files:
+        print("the request method was POST")
+        if 'file_1' not in request.files or 'file_2' not in request.files:
+            print("either file_1 or file_2 was not included in the request")
             return redirect(request.url)
 
-        file = request.files['file']
+        file_1 = request.files['file_1']
+        file_2 = request.files['file_2']
 
-        if file.filename == '':
+        if file_1.filename == '' or file_2.filename == '':
+            print("one of the filenames was empty")
             return redirect(request.url)
 
-        if file and allowed_file(file.filename):
+        if file_1 and allowed_file(file_1.filename) and file_2 and allowed_file(file_2.filename):
+            print("the file type is an allowed type")
             # The image file seems valid! Detect faces and return the result.
-            return detect_faces_in_image(file)
+            return detect_faces_in_image(file_1, file_2)
 
+    print("no valid image file was uploaded")
     # If no valid image file was uploaded, show the file upload form:
     return '''
     <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <title>Are these the same picture?</title>
+    <h1>Upload pictures and see if they are the same person!</h1>
     <form method="POST" enctype="multipart/form-data">
-      <input type="file" name="file">
+      <input type="file" name="file_1">
+      <input type="file" name="file_2">
       <input type="submit" value="Upload">
     </form>
     '''
 
 
-def detect_faces_in_image(file_stream):
-    # Pre-calculated face encoding of Obama generated with face_recognition.face_encodings(img)
-    known_face_encoding = [-0.09634063,  0.12095481, -0.00436332, -0.07643753,  0.0080383,
-                            0.01902981, -0.07184699, -0.09383309,  0.18518871, -0.09588896,
-                            0.23951106,  0.0986533 , -0.22114635, -0.1363683 ,  0.04405268,
-                            0.11574756, -0.19899382, -0.09597053, -0.11969153, -0.12277931,
-                            0.03416885, -0.00267565,  0.09203379,  0.04713435, -0.12731361,
-                           -0.35371891, -0.0503444 , -0.17841317, -0.00310897, -0.09844551,
-                           -0.06910533, -0.00503746, -0.18466514, -0.09851682,  0.02903969,
-                           -0.02174894,  0.02261871,  0.0032102 ,  0.20312519,  0.02999607,
-                           -0.11646006,  0.09432904,  0.02774341,  0.22102901,  0.26725179,
-                            0.06896867, -0.00490024, -0.09441824,  0.11115381, -0.22592428,
-                            0.06230862,  0.16559327,  0.06232892,  0.03458837,  0.09459756,
-                           -0.18777156,  0.00654241,  0.08582542, -0.13578284,  0.0150229 ,
-                            0.00670836, -0.08195844, -0.04346499,  0.03347827,  0.20310158,
-                            0.09987706, -0.12370517, -0.06683611,  0.12704916, -0.02160804,
-                            0.00984683,  0.00766284, -0.18980607, -0.19641446, -0.22800779,
-                            0.09010898,  0.39178532,  0.18818057, -0.20875394,  0.03097027,
-                           -0.21300618,  0.02532415,  0.07938635,  0.01000703, -0.07719778,
-                           -0.12651891, -0.04318593,  0.06219772,  0.09163868,  0.05039065,
-                           -0.04922386,  0.21839413, -0.02394437,  0.06173781,  0.0292527 ,
-                            0.06160797, -0.15553983, -0.02440624, -0.17509389, -0.0630486 ,
-                            0.01428208, -0.03637431,  0.03971229,  0.13983178, -0.23006812,
-                            0.04999552,  0.0108454 , -0.03970895,  0.02501768,  0.08157793,
-                           -0.03224047, -0.04502571,  0.0556995 , -0.24374914,  0.25514284,
-                            0.24795187,  0.04060191,  0.17597422,  0.07966681,  0.01920104,
-                           -0.01194376, -0.02300822, -0.17204897, -0.0596558 ,  0.05307484,
-                            0.07417042,  0.07126575,  0.00209804]
+def detect_faces_in_image(file_stream_1, file_stream_2):
+    # Pre-calculated face encoding of file_1 generated with face_recognition.face_encodings(known_face_image)
+    img_1 = face_recognition.load_image_file(file_stream_1)
+    known_face_encodings = face_recognition.face_encodings(img_1)
 
     # Load the uploaded image file
-    img = face_recognition.load_image_file(file_stream)
+    img_2 = face_recognition.load_image_file(file_stream_2)
     # Get face encodings for any faces in the uploaded image
-    unknown_face_encodings = face_recognition.face_encodings(img)
+    unknown_face_encodings = face_recognition.face_encodings(img_2)
 
     face_found = False
-    is_obama = False
+    is_same = False
 
-    if len(unknown_face_encodings) > 0:
+    if len(unknown_face_encodings) > 0 and len(known_face_encodings) > 0:
         face_found = True
-        # See if the first face in the uploaded image matches the known face of Obama
-        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0])
+        # See if the first face in the uploaded image matches the known face
+        match_results = face_recognition.compare_faces([known_face_encodings[0]], unknown_face_encodings[0])
         if match_results[0]:
-            is_obama = True
+            is_same = True
 
     # Return the result as json
     result = {
-        "face_found_in_image": face_found,
-        "is_picture_of_obama": is_obama
+        "faces_found_in_images": face_found,
+        "is_same_person": is_same
     }
     return jsonify(result)
 
